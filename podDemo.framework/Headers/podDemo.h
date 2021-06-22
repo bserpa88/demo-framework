717e968aac1223b627289a964dce8b027fa207d2
Bryan Serpa <bryan.serpa@honeywell.com>
2021-06-22 14:53:03 -0700

    adding framework for 0.0.1

---
+++ podDemo.framework/Headers/podDemo.h
@@ -0,0 +1,19 @@
+//
+//  podDemo.h
+//  podDemo
+//
+//  Created by H363660 on 6/22/21.
+//  Copyright © 2021 H363660. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for podDemo.
+FOUNDATION_EXPORT double podDemoVersionNumber;
+
+//! Project version string for podDemo.
+FOUNDATION_EXPORT const unsigned char podDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podDemo/PublicHeader.h>
+#import <podDemo/podDemo-swift.h>
+
